320ce6854d3bfc150f7961c25a907a41f99a2213
Andreas Maunz <andreas@maunz.de>
2012-11-20 16:12:02 +0100

    Removed debug

--- run_fminer.rb
+++ run_fminer.rb
@@ -47,7 +47,7 @@ fsm=true
 all_cas=table.collect { |row| row["CAS"] } # collect results later: use occ-1 to access all_cas index
 puts "AM: #{all_cas.join(', ')}" if do_debug 
 puts "AM: #{all_cas.length}" if do_debug 
-puts
+
 
 
 output=$myFminer.run_fminer(table, $endpoint, {:min_freq => min_freq, :fsm => fsm})
@@ -59,38 +59,17 @@ occ_smarts=(1..all_cas.length).to_a.inject({}) { |h,idx|
 }
 
 
+
 puts "AM: #{occ_smarts.inspect}" if do_debug
 puts "AM: #{occ_smarts.length}" if do_debug
-puts
 
 patterns.each { |p|
   smarts=p[0]
   all_smarts = all_smarts.add smarts; 
   occ_pos=p[2]; occ_neg=p[3] # Assumes only two classes
   occs = (occ_pos + occ_neg).sort
-  occs.each { |o|
-    occ_smarts[o.to_i][smarts] = 1
-  }
+  occs.each { |o| occ_smarts[o.to_i][smarts] = 1 }
 }
-#puts "AM: #{occ_smarts.inspect}" if do_debug
-
-puts
-puts "AM: --- #{occ_smarts[0].inspect}" if do_debug
-puts "AM: #{occ_smarts[1].inspect}" if do_debug
-puts "AM: #{occ_smarts[2].inspect}" if do_debug
-puts "AM: --- #{occ_smarts[3].inspect}" if do_debug
-puts
-
-#puts
-#puts "AM: --- #{occ_smarts[259].inspect}" if do_debug
-#puts "AM: #{occ_smarts[260].inspect}" if do_debug
-#puts "AM: #{occ_smarts[261].inspect}" if do_debug
-#puts "AM: #{occ_smarts[262].inspect}" if do_debug
-#puts "AM: --- #{occ_smarts[263].inspect}" if do_debug
-#puts
-#puts "AM: #{occ_smarts.length}" if do_debug
-#puts
-
 
 header = ["CAS"]
 header << all_smarts.to_a # This will give order of features for filling table
@@ -101,16 +80,14 @@ final_table = []
 (1..all_cas.length).to_a.each { |occ_idx|
   line=Array.new
   line<<all_cas[occ_idx-1]
-  (1..(header.size-1)).each { |i|
+  (1...header.size).each { |i|
     line << (occ_smarts[occ_idx].has_key?(header[i]) ? 1 : 0)
   }
   final_table << line
 }
 
 csv_str = ""
-final_table.each { |line|
-  csv_str << line.join(',') << "\n"
-}
+final_table.each { |line| csv_str << line.join(',') << "\n" }
 
 File.open($output_file, 'w') do |f|
   f.puts header_str
